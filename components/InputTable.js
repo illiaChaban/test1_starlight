d564ed1ff94d27c066e758515fa512fd6f31a88e
Illia Chaban <illia.chaban.8@gmail.com>
2018-06-11 13:44:19 -0400

    more refactored

--- components/InputTable.js
+++ components/InputTable.js
@@ -17,46 +17,49 @@ let TableCell = ({name, handleChange, value }) =>
         </input>
     </td>
 
-class TableRow extends React.Component{
-    render() {
 
-        let { index, handleChange } = this.props;
-        return (
-                <tr>
-                    { columns.map( (name,i) => {
-                        return <TableCell 
-                                    key={i}
-                                    name={name} 
-                                    handleChange={(e)=> handleChange(e, index)}
-                                    />
-                    })}
-                </tr>
-        )
-    }
+let TableRow = ({ index, handleChange }) =>
+    <tr>
+        { columns.map( (name,i) => {
+            return <TableCell 
+                        key={i}
+                        name={name} 
+                        handleChange={(e)=> handleChange(e, index)}
+                        />
+        })}
+    </tr>
+// class TableRow extends React.Component{
+//     render() {
+//         let { index, handleChange } = this.props;
+//         return (
+//                 <tr>
+//                     { columns.map( (name,i) => {
+//                         return <TableCell 
+//                                     key={i}
+//                                     name={name} 
+//                                     handleChange={(e)=> handleChange(e, index)}
+//                                     />
+//                     })}
+//                 </tr>
+//         )
+//     }
+// } 
 
-
-} 
-
-
+const rowNum = 2;
 
 class InputTable extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            rowNum: 2,
-            data: [],
+            rowNum,
+            data: Array(rowNum).fill({}),
         }
     }
 
     render() {
         let { rowNum, data } = this.state;
         let { showCalculations, updateData } = this.props;
-        let addRow = () => this.setState({ rowNum: rowNum + 1})
-        let addObj = () => {
-            let data = [ ...this.state.data];
-            data.push({})
-            this.setState({ data})
-        }
+        let addRow = () => this.setState({ rowNum: rowNum + 1, })
         let handleChange = (event, index) => {
             let newData = [ ...this.state.data];
             if (!newData[index]) newData[index] = {}; 
